bca7462c95f87dee827b656b92cf40536183cbcd
Recep ALICI <alicirecepp@gmail.com>
2024-07-12 21:59:36 +0200

    firstsession

---
+++ src/test/java/stepDefinitions/Grid_02.java
@@ -0,0 +1,45 @@
+package stepDefinitions;
+
+import manage.DriverManage;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Grid_02 {
+
+	DriverManage driverManage = new DriverManage();
+	static WebDriver driver;
+
+	@Test
+	void chromeTest(){
+
+		driver = driverManage.setupChromeDriver();
+		driver.get("https://www.ebay.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+
+
+	}
+
+
+	@Test
+	void firefoxTest(){
+
+		driver=driverManage.setupFirefoxDriver();
+		driver.get("https://www.youtube.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+
+	}
+
+
+	@Test
+	void edgeTest(){
+
+		driver = driverManage.setupEdgeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+
+	}
+
+}
